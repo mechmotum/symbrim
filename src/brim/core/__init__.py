2fe680855fffa30255f2951ffe5c1baf8c668745
TJStienstra <timostienstra00@gmail.com>
2023-04-13 21:27:16 +0200

    Implement singleton

--- src/brim/core/__init__.py
+++ src/brim/core/__init__.py
@@ -1,6 +1,7 @@
 """Module containing the core elements of BRiM."""
 
 __all__ = [
+    "Singleton",
     "Requirement",
     "ConnectionMeta", "ModelMeta",
     "ConnectionBase", "ModelBase",
@@ -10,3 +11,4 @@ __all__ = [
 from brim.core.mixins import NewtonianBodyMixin
 from brim.core.model_base import ConnectionBase, ConnectionMeta, ModelBase, ModelMeta
 from brim.core.requirement import Requirement
+from brim.core.singleton import Singleton
---
+++ tests/core/test_singleton.py
@@ -0,0 +1,23 @@
+import pytest
+from brim.core import Singleton
+
+
+class TestSingleton:
+    @pytest.fixture(autouse=True)
+    def _setup(self) -> None:
+        class TestClass(Singleton):
+            pass
+
+        self.TestClass = TestClass
+
+    def test_singleton(self) -> None:
+        assert self.TestClass() is self.TestClass()
+
+    def test_singleton_activate(self) -> None:
+        instance = self.TestClass()
+        instance.deactivate()
+        instance2 = self.TestClass()
+        assert instance2 is self.TestClass()
+        assert instance2 is not instance
+        instance.activate()
+        assert instance is self.TestClass()
