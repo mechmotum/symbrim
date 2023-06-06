42b08f7d58194136db7a9ea22508b147a7bfa1d6
TJStienstra <timostienstra00@gmail.com>
2023-06-06 11:31:26 +0200

    Implement a method to get all possible methods that satisfy a requirement

--- src/brim/core/registry.py
+++ src/brim/core/registry.py
@@ -1,6 +1,7 @@
 """Registry to keep track of all existing model and connection types in BRiM."""
 from __future__ import annotations
 
+from brim.core.requirement import ConnectionRequirement, ModelRequirement
 from brim.core.singleton import Singleton
 
 __all__ = ["Registry"]
@@ -30,3 +31,38 @@ class Registry(Singleton):
     def connections(self) -> frozenset[type]:
         """Return the registered connections."""
         return frozenset(self._connections)
+
+    def get_from_requirement(self,
+                             requirement: ConnectionRequirement | ModelRequirement,
+                             drop_abstract: bool = True
+                             ) -> list[type]:
+        """Return all models or connections that satisfy the given requirement.
+
+        Parameters
+        ----------
+        requirement : ConnectionRequirement | ModelRequirement
+            The requirement to satisfy.
+        drop_abstract : bool, optional
+            Whether to drop abstract classes, by default True.
+
+        Returns
+        -------
+        list[type]
+            All models or connections that satisfy the given requirement.
+        """
+        if isinstance(requirement, ModelRequirement):
+            options = [
+                model for model in self.models if requirement.is_satisfied_by(model)
+            ]
+        elif isinstance(requirement, ConnectionRequirement):
+            options = [
+                conn for conn in self.connections if requirement.is_satisfied_by(conn)
+            ]
+        else:
+            raise TypeError(
+                f"Expected requirement to be of type {ModelRequirement} or "
+                f"{ConnectionRequirement}, but got {type(requirement)} instead."
+            )
+        if drop_abstract:
+            options = [option for option in options if option.__name__[-4:] != "Base"]
+        return options
--- tests/core/test_registry.py
+++ tests/core/test_registry.py
@@ -1,6 +1,13 @@
 import pytest
-from brim.bicycle import KnifeEdgeWheel, NonHolonomicTyre, ToroidalWheel, WheelBase
+from brim.bicycle import (
+    KnifeEdgeWheel,
+    NonHolonomicTyre,
+    ToroidalWheel,
+    TyreBase,
+    WheelBase,
+)
 from brim.core import Registry
+from brim.core.requirement import ConnectionRequirement, ModelRequirement
 from brim.other.rolling_disc import RollingDisc
 
 
@@ -16,3 +23,29 @@ class TestRegistry:
 
     def test_connections_and_models_split(self) -> None:
         assert Registry().models.isdisjoint(Registry().connections)
+
+    @pytest.mark.parametrize("requirement, subset, disjoint", [
+        (ModelRequirement("wheel", WheelBase, "Wheel model."),
+         {KnifeEdgeWheel, ToroidalWheel},
+         {NonHolonomicTyre, RollingDisc, WheelBase}),
+        (ConnectionRequirement("tyre", TyreBase, "Tyre model."),
+         {NonHolonomicTyre},
+         {KnifeEdgeWheel, ToroidalWheel, TyreBase, RollingDisc, WheelBase}),
+    ])
+    def test_get_from_requirement_default(self, requirement, subset, disjoint) -> None:
+        assert subset.issubset(Registry().get_from_requirement(requirement))
+        assert disjoint.isdisjoint(Registry().get_from_requirement(requirement))
+
+    @pytest.mark.parametrize("args, kwargs, subset, disjoint", [
+        ((ModelRequirement("wheel", WheelBase, "Wheel model."),),
+         {"drop_abstract": False},
+         {KnifeEdgeWheel, ToroidalWheel, WheelBase},
+         {NonHolonomicTyre, RollingDisc}),
+        ((ConnectionRequirement("tyre", TyreBase, "Tyre model."),),
+         {"drop_abstract": False},
+         {NonHolonomicTyre, TyreBase},
+         {KnifeEdgeWheel, ToroidalWheel, RollingDisc, WheelBase}),
+    ])
+    def test_get_from_requirement_custom(self, args, kwargs, subset, disjoint) -> None:
+        assert subset.issubset(Registry().get_from_requirement(*args, **kwargs))
+        assert disjoint.isdisjoint(Registry().get_from_requirement(*args, **kwargs))
