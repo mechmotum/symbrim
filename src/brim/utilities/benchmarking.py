9528caf9fcabfec4054f69ceae3b76671515a828
TJStienstra <timostienstra00@gmail.com>
2023-07-01 20:27:10 +0200

    Implement a benchmark utility function

---
+++ src/brim/utilities/benchmarking.py
@@ -0,0 +1,47 @@
+"""Module containing utilities to easily benchmark models."""
+
+from typing import Optional
+
+import pytest
+from sympy import count_ops, cse
+from sympy.core.cache import clear_cache
+
+__all__ = ["benchmark"]
+
+
+def benchmark(rounds: int = 3, group: Optional[str] = None):  # pragma: no cover
+    """Create decorator to benchmark a function.
+
+    Parameters
+    ----------
+    rounds : int, optional
+        Number of rounds to run the benchmark for, by default 3.
+    group : Optional[str], optional
+        Group to put the benchmark in, by default None.
+
+    Returns
+    -------
+    function
+        Decorated function.
+
+    """
+
+    def decorator(func):
+        @pytest.mark.benchmark(group=group)
+        def wrapper(benchmark):
+            data = {}
+
+            def setup():
+                clear_cache()
+                data["system"] = func()
+
+            def form_eoms():
+                data["eoms"] = data["system"]._form_eoms()
+
+            benchmark.pedantic(form_eoms, setup=setup, rounds=rounds)
+            benchmark.extra_info["operation_eoms"] = count_ops(data["eoms"])
+            benchmark.extra_info["operation_eoms_csed"] = count_ops(cse(data["eoms"]))
+
+        return wrapper
+
+    return decorator
