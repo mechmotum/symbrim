ec02a0a7852b15de2f37e0630e2af962f69bba9d
TJStienstra <timostienstra00@gmail.com>
2023-07-03 00:23:51 +0200

    Include setup in benchmark and add kwarg support

--- src/brim/utilities/benchmarking.py
+++ src/brim/utilities/benchmarking.py
@@ -9,7 +9,7 @@ from sympy.core.cache import clear_cache
 __all__ = ["benchmark"]
 
 
-def benchmark(rounds: int = 3, group: Optional[str] = None):
+def benchmark(rounds: int = 3, group: Optional[str] = None, **kwargs) -> callable:
     """Create decorator to benchmark a function.
 
     Parameters
@@ -18,11 +18,16 @@ def benchmark(rounds: int = 3, group: Optional[str] = None):
         Number of rounds to run the benchmark for, by default 3.
     group : Optional[str], optional
         Group to put the benchmark in, by default None.
+    kwargs
+        If kwargs are provided, then they are passed to
+        :meth:`sympy.physics.mechanics._system.System.form_eoms`.
 
     Returns
     -------
     function
-        Decorated function.
+        Decorated function, which should return an instance of
+        :class:`sympy.physics.mechanics._system.System` or
+        :class:`sympy.physics.mechanics.kane.KanesMethod`.
 
     """
 
@@ -33,11 +38,13 @@ def benchmark(rounds: int = 3, group: Optional[str] = None):
 
             def setup():
                 clear_cache()
-                data["system"] = func()
 
             def form_eoms():
-                data["eoms"] = data["system"]._form_eoms()
-
+                data["system"] = func()
+                if hasattr(data["system"], "form_eoms"):
+                    data["eoms"] = data["system"].form_eoms(**kwargs)
+                else:
+                    data["eoms"] = data["system"]._form_eoms()
             benchmark.pedantic(form_eoms, setup=setup, rounds=rounds)
             benchmark.extra_info["operation_eoms"] = count_ops(data["eoms"])
             benchmark.extra_info["operation_eoms_csed"] = count_ops(cse(data["eoms"]))
