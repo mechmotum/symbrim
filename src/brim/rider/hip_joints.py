9e5df76d5dd9f679d7fbaefa0e33328c655941e3
TJStienstra <timostienstra00@gmail.com>
2023-07-07 18:51:05 +0200

    Change hip back from abduction to adduction

--- src/brim/rider/hip_joints.py
+++ src/brim/rider/hip_joints.py
@@ -28,10 +28,10 @@ class SphericalHipMixin:
         return {
             **super().descriptions,
             self.q[0]: "Flexion angle of the hip.",
-            self.q[1]: "Abduction angle of the hip.",
+            self.q[1]: "Adduction angle of the hip.",
             self.q[2]: "Endorotation angle of the hip.",
             self.u[0]: "Flexion angular velocity of the hip.",
-            self.u[1]: "Abduction angular velocity of the hip.",
+            self.u[1]: "Adduction angular velocity of the hip.",
             self.u[2]: "Rotation angular velocity of the hip.",
         }
 
@@ -39,9 +39,9 @@ class SphericalHipMixin:
         """Define the objects."""
         super()._define_objects()
         self.q = Matrix(
-            dynamicsymbols(self._add_prefix("q_flexion, q_abduction, q_rotation")))
+            dynamicsymbols(self._add_prefix("q_flexion, q_adduction, q_rotation")))
         self.u = Matrix(
-            dynamicsymbols(self._add_prefix("u_flexion, u_abduction, u_rotation")))
+            dynamicsymbols(self._add_prefix("u_flexion, u_adduction, u_rotation")))
         self._system = System.from_newtonian(self.pelvis.body)
 
 
@@ -56,7 +56,7 @@ class SphericalLeftHip(SphericalHipMixin, LeftHipBase):
                 self._add_prefix("joint"), self.pelvis.body, self.leg.hip, self.q,
                 self.u, self.pelvis.left_hip_point, self.leg.hip_interpoint,
                 self.pelvis.frame, self.leg.hip_interframe, rot_type="BODY",
-                amounts=(self.q[0], self.q[1], self.q[2]), rot_order="YXZ")
+                amounts=(self.q[0], -self.q[1], self.q[2]), rot_order="YXZ")
         )
 
 
@@ -71,7 +71,7 @@ class SphericalRightHip(SphericalHipMixin, RightHipBase):
                 self._add_prefix("joint"), self.pelvis.body, self.leg.hip, self.q,
                 self.u, self.pelvis.right_hip_point, self.leg.hip_interpoint,
                 self.pelvis.frame, self.leg.hip_interframe, rot_type="BODY",
-                amounts=(self.q[0], -self.q[1], -self.q[2]), rot_order="YXZ")
+                amounts=(self.q[0], self.q[1], -self.q[2]), rot_order="YXZ")
         )
 
 
@@ -126,27 +126,27 @@ class SphericalHipTorque(LoadGroupBase):
         return {
             **super().descriptions,
             self.symbols["T_flexion"]: "Flexion torque of the hip.",
-            self.symbols["T_abduction"]: "Abduction torque of the hip.",
+            self.symbols["T_adduction"]: "Adduction torque of the hip.",
             self.symbols["T_rotation"]: "Rotation torque of the hip.",
         }
 
     def _define_objects(self) -> None:
         """Define the objects."""
         self.symbols.update({name: dynamicsymbols(self._add_prefix(name)) for name in (
-            "T_flexion", "T_abduction", "T_rotation")})
+            "T_flexion", "T_adduction", "T_rotation")})
 
     def _define_loads(self) -> None:
         """Define the loads."""
         hip = self.parent.system.joints[0]
-        abduction_axis = (cos(hip.coordinates[0]) * hip.parent_interframe.x -
+        adduction_axis = (cos(hip.coordinates[0]) * hip.parent_interframe.x -
                           sin(hip.coordinates[0]) * hip.parent_interframe.z)
         if isinstance(self.parent, RightHipBase):
-            abduction_axis *= -1
             rot_dir = -1
         else:
+            adduction_axis *= -1
             rot_dir = 1
         torque = (self.symbols["T_flexion"] * hip.parent_interframe.y +
-                  self.symbols["T_abduction"] * abduction_axis +
+                  self.symbols["T_adduction"] * adduction_axis +
                   self.symbols["T_rotation"] * rot_dir * hip.child_interframe.z)
         self.parent.system.add_loads(
             Torque(hip.child_interframe, torque),
@@ -164,7 +164,7 @@ class SphericalHipSpringDamper(LoadGroupBase):
     def descriptions(self) -> dict[Any, str]:
         """Descriptions of the objects."""
         desc = {**super().descriptions}
-        for tp in ("flexion", "abduction", "rotation"):
+        for tp in ("flexion", "adduction", "rotation"):
             desc.update({
                 self.symbols[f"k_{tp}"]:
                     f"{tp.capitalize()} stiffness of hip: {self.parent}.",
@@ -177,27 +177,27 @@ class SphericalHipSpringDamper(LoadGroupBase):
 
     def _define_objects(self) -> None:
         """Define the objects."""
-        for tp in ("flexion", "abduction", "rotation"):
+        for tp in ("flexion", "adduction", "rotation"):
             self.symbols.update({name: dynamicsymbols(self._add_prefix(name))
                                  for name in (f"k_{tp}", f"c_{tp}", f"q_ref_{tp}")})
 
     def _define_loads(self) -> None:
         """Define the loads."""
         hip = self.parent.system.joints[0]
-        abduction_axis = (cos(hip.coordinates[0]) * hip.parent_interframe.x -
+        adduction_axis = (cos(hip.coordinates[0]) * hip.parent_interframe.x -
                           sin(hip.coordinates[0]) * hip.parent_interframe.z)
         if isinstance(self.parent, RightHipBase):
-            abduction_axis *= -1
             rot_dir = -1
         else:
+            adduction_axis *= -1
             rot_dir = 1
         torques = []
-        for i, tp in enumerate(("flexion", "abduction", "rotation")):
+        for i, tp in enumerate(("flexion", "adduction", "rotation")):
             torques.append(-self.symbols[f"k_{tp}"] * (
                     hip.coordinates[i] - self.symbols[f"q_ref_{tp}"]) -
                            self.symbols[f"c_{tp}"] * hip.speeds[i])
         torque = (torques[0] * hip.parent_interframe.y +
-                  torques[1] * abduction_axis +
+                  torques[1] * adduction_axis +
                   torques[2] * rot_dir * hip.child_interframe.z)
         self.parent.system.add_loads(
             Torque(hip.child_interframe, torque),
